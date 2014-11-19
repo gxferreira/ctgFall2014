5f51f8eb8239c0b43ff7893c15911d5f4eadf5b0
lariteixeira <larissateixeira92@gmail.com>
2014-11-18 19:27:54 -0500

    new index

--- js/exercises.js
+++ js/exercises.js
@@ -1,6 +1,8 @@
-var weight, metric = 0, calories = [];
- 
+var weight, metric = 0, calories = [], barChartData = [];
+var color = ["#b22200","#EACE3F"];
+
 function exercises_function(){
+
   var div = '.exercises';
 
   $(div).empty();
@@ -9,19 +11,28 @@ function exercises_function(){
 
   weight = calculateWeight();
 
+  barChartData = new Array();
+
   $.getJSON( "secure/exercises.json", function(data) {
 
     $.each(data.exercises, function(key, value){
 
       $(div).append(
-        '<div class="col">'+
+        '<div class="large-3 medium-3 columns">'+
         '<h3 id="'+key+'">'+key+'</h3>'+
         '<img src="'+value.image+'"></img>'+
         '</div>');
       
-      calculateExercises(value.met, key);
+      for(var i in calories){
+        var minutes = calculateExercises(value.met, key, calories[i], i);
+        createBarData(i, key, minutes, value.image, value.group, value.color);
+      }
+
 
   });
+
+createBarChart();
+  
 
   }).fail(function() {
   		alert('File Not Found');
@@ -29,18 +40,19 @@ function exercises_function(){
 
 };
 
-function calculateExercises(met, key){
+function calculateExercises(met, key, calorie,i){
   var minutes = 0;
-  for( var i in calories){
-    minutes = Math.round((calories[i] * 60) / (weight * met));
-    $('<p>'+minutes+' min</p>').insertAfter('#'+key);
-  }
+  //for( var i in calories){
+    minutes = Math.round((calorie * 60) / (weight * met));
+    $('<p style="color:'+color[i]+'">'+minutes+' min</p>').insertAfter('#'+key);
+  //}
+  return minutes;
 }
 
 function calculateWeight(){
-	weight = $('#weight').val();	
+	weight = $('#weight').val();
 	metric = $('#weight-metric').val();
-	weight *= metric;
+	weight /= metric;
 	return weight;
 }
 
@@ -49,4 +61,29 @@ function getCalories(){
   for(i=0; i<foodData.length; i++){
     calories.push(foodData[i].report.food.nutrients[1].measures[0].value);
   }
+}
+
+function createBarData(i, exercise, minutes, img, group, color){
+  i++;
+  barChartData.push({"exercise":exercise, "name":"food"+i, "minutes":minutes, "image":img, "group": group, "color":color});
+  console.log(barChartData);
+
+}
+
+function createBarChart(){
+   var visualization = d3plus.viz()
+    .container("#viz")
+    .data(barChartData)
+    .type("bar")
+    .id("name")
+    .x("group")
+    .y("minutes")
+    .color("color")
+    .icon({
+      "style": "knockout",
+      "value": "image"
+    })
+    .tooltip("exercise")
+    .legend(false)
+    .draw()
 }
